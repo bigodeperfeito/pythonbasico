a3779fdf5a4d6f9e2229c0c77e1265bc549da65f
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2026-02-09 14:11:52 +0000

    t

---
+++ ola.py
@@ -0,0 +1,7 @@
+#nome = input("Digite seu nome: ")
+#print("Olá, ",nome, "Seja bem vindo ")
+
+nome = input ("Digite seu nome: ")
+sobrenome = input ("Digite seu sobronome: ")
+
+print("Olá," ,"seu nome é", nome, sobrenome)
