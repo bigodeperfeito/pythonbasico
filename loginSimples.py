480d1956d51c94430ec91b900d33a33bf225f538
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2026-02-10 13:37:00 +0000

    i

--- loginSimples.py
+++ loginSimples.py
@@ -1,6 +1,5 @@
 while True:
     nome= input ("Digite seu nome:")
-    senha = input ("Digite sua senha:")
 
     if nome=="":
         print("O nome não pode ser vazio.")
@@ -9,10 +8,10 @@ while True:
     
 print("Nome",nome)
 
-while true:
-        senha = input ("Digite sua senha: (max. 0 caracterers):")
+while True:
+        senha = input ("Digite sua senha: (max. 6 caracterers):")
 
-        if len(senha)<=0:
+        if len(senha)<=6:
             break
         else:
             print("A senha deve ter 6 caracters no máximo")
