a17a39751b65ecdc6b2b52b9b628aed51d568ce2
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2026-02-24 12:08:58 +0000

    versao02

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,17 +1,33 @@
+import random
+
 print ('***********************')
 print('****Jogo adivinhação****')
 print('************************')
 
-numero_secreto = 39
+numero_secreto = random.randint(1, 100)
+total_tentativas = 5
+rodada = 1
 
-chute_str = input("Digite o seu numero:")
+while(rodada <= total_tentativas):
+    chute_str = input("Digite o seu numero de 1 a 100: ")
+    print("Seu numero é:", chute_str)
 
-print("Seu numero é:", chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(numero_secreto == chute):
-    print("Você Acertou!!")
+    if(acertou):
+        print("Você Acertou!!")
+        break
 
-else:
-    print("Você errou :(")
+    else:
+        if(maior):
+            print("O seu chute foi maior que o numero secreto")
+        elif (menor):
+            print("O seu chute foi menor que o numero secreto")
+    rodada = rodada +1
+
+print(numero_secreto)
+print("Fim de jogo!")
